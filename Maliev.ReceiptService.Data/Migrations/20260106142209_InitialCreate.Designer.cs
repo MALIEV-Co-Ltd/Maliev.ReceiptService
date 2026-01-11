06cdd982a888f3b79eab56d3c538b180c69bf933
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-01-11 19:11:23 +0700

    feat: refresh initial database migration
    Refresh initial migration with correct timestamp and updated entity definitions for stabilization.

--- Maliev.ReceiptService.Data/Migrations/20260106142209_InitialCreate.Designer.cs
+++ Maliev.ReceiptService.Data/Migrations/20260106142209_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Maliev.ReceiptService.Data.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Maliev.ReceiptService.Data.Migrations
 {
     [DbContext(typeof(ReceiptDbContext))]
-    partial class ReceiptDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260106142209_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
